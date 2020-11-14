976978ecaa438d612b10db3947f7619dfd75eadf
Jed <olivier.begue.begue@gmail.com>
2020-11-14 21:50:11 +0100

    endgame

---
+++ Assets/Scripts/Endgame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Endgame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SceneManager.LoadScene("Main Menu");
+        StartCoroutine("reloadMenu", 30f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator reloadMenu(float time)
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene("Main Menu");
+    }
+}
