d0bf7bc95438f8baf17fff2827d00a7e9559afe9
Gwendoline <gwendoline.serandour@telecom-sudparis.eu>
2020-11-14 15:10:56 +0100

    possibilité de modifier texte + image

---
+++ Assets/Scripts/JT/ChangeTextAndImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class ChangeTextAndImage : MonoBehaviour {
+   [SerializeField] 
+   private Image image;
+
+   [SerializeField] 
+   private Text infoTitle;
+   
+   [SerializeField] 
+   private Text scrollingText;
+   
+   public void setScrollingText(String text) {
+      scrollingText.text = text;
+   }
+
+   public void setInfoTitle(String text) {
+      infoTitle.text = text;
+   }
+
+   public void setImage(Sprite img) {
+      image.sprite = img;
+   }
+
+}
