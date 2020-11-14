749772e9063ca0b62e7101e30f95e0a46292ed07
jglrxavpok <jglrxavpok@gmail.com>
2020-11-15 00:18:10 +0100

    Glow shader

---
+++ Assets/Scripts/Player/ExposeSpriteToShader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Player {
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ExposeSpriteToShader : MonoBehaviour {
+
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer;
+        [SerializeField]
+        private Material shader;
+
+        private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+
+        private void Update() {
+            var texture = _spriteRenderer.sprite.texture;
+            shader.SetTexture(MainTex, texture);
+        }
+    }
+}
