8a325c750cfbcf466a6a2d026f8ee293b45d59a1
rene-INTech <remi.lamiaux@telecom-sudparis.eu>
2020-11-14 10:44:53 +0100

    Pnj creation (random name)

---
+++ Assets/Scripts/Pnj.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pnj : MonoBehaviour
+{
+    public string thoughts;
+    public char gender; //'M' or 'F'
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
