3e74d01d899089184ecd3170d0e9826442ca19d3
rene-INTech <remi.lamiaux@telecom-sudparis.eu>
2020-11-14 18:57:49 +0100

    random generated names integrated to NPC

--- Assets/Scripts/Pnj/PnjScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Pnj/Data", menuName = "ScriptableObjects/PnjScriptableObject", order = 1)]
-public class PnjScriptableObject : ScriptableObject
-{
-   public string[] thoughts;
-}
