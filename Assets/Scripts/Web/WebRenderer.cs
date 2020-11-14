d86d5f8783a4c8812e6ddedf6931e75e20e1772c
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-11-14 10:45:41 +0100

    web renderer in progress

---
+++ Assets/Scripts/Web/WebRenderer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebRenderer : MonoBehaviour
+{
+    public int nbPoint = 50;
+
+    private LineRenderer line;
+
+    private Vector3 endPoint;
+    private bool show = false;
+
+    private void Awake () {
+        line = GetComponent<LineRenderer>();
+
+        line.positionCount = nbPoint;
+    }
+
+    private void Update () {
+        if(!show) return;
+
+        UpdateLine();
+    }
+
+    public void CreateWeb (Vector3 endPoint) {
+        this.endPoint = endPoint;
+        line.SetPosition(nbPoint - 1, endPoint);
+
+        UpdateLine();
+
+        show = true;
+    }
+
+    public void DestroyWeb() {
+        show = false;
+    }
+
+    private void UpdateLine () {
+        line.SetPosition(0, transform.position);
+
+        float distance = Vector3.Distance(transform.position, endPoint);
+        Vector3 center = transform.position + transform.up * distance / 2;
+
+        for(int i = 1; i < nbPoint - 1; ++i){
+            float t = (float)i / (nbPoint - 1);
+            line.SetPosition(i, GetPoint(transform.position, center, endPoint, t));
+        }
+
+        //line.SetPosition(1, GetPoint(transform.position, center.position, endPoint, (float)2/3));
+        //line.SetPosition(2, GetPoint(transform.position, center.position, endPoint, (float)1/3));
+    } 
+
+    public static Vector3 GetPoint (Vector3 p0, Vector3 p1, Vector3 p2, float t) {
+		t = Mathf.Clamp01(t);
+		float oneMinusT = 1f - t;
+		return
+			oneMinusT * oneMinusT * p0 +
+			2f * oneMinusT * t * p1 +
+			t * t * p2;
+	}
+}
