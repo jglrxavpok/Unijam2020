c2a4802085462b5181c81b85784d6671a3877fbe
jglrxavpok <jglrxavpok@gmail.com>
2020-11-14 21:10:52 +0100

    Morsures restantes, plus de NPC

---
+++ Assets/Scripts/UI/RemainingBitesDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    [RequireComponent(typeof(Text))]
+    public class RemainingBitesDisplay : MonoBehaviour {
+
+        [SerializeField] private string prefix;
+        private Text _text;
+
+        private void Start() {
+            _text = GetComponent<Text>();
+            BitingSystem.Instance.BiteCountChange += UpdateText;
+        }
+
+        private void OnDestroy() {
+            BitingSystem.Instance.BiteCountChange -= UpdateText;
+        }
+
+        private void UpdateText(int count) {
+            _text.text = $"{prefix} {count}";
+        }
+    }
+}
