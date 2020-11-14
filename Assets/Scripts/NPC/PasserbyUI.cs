88dc6f5a7e94812631944fd0ff22079da7405818
jglrxavpok <jglrxavpok@gmail.com>
2020-11-14 18:47:04 +0100

    Différents types de pensée dans le menu (non accessibles encore)

--- Assets/Scripts/NPC/PasserbyUI.cs
+++ Assets/Scripts/NPC/PasserbyUI.cs
@@ -3,9 +3,16 @@ using UnityEngine.UI;
 
 namespace NPC {
     public class PasserbyUI : MonoBehaviour {
-        [SerializeField] private Text fullName;
-        [SerializeField] private Text thought;
-        [SerializeField] private Image facePicture;
+        [SerializeField] private Text fullName = null;
+        [SerializeField] private GameObject tasteGroup = null;
+        [SerializeField] private Text thoughtTaste = null;
+        [SerializeField] private GameObject intentionGroup = null;
+        [SerializeField] private Text thoughtIntention = null;
+        [SerializeField] private GameObject judgmentGroup = null;
+        [SerializeField] private Text thoughtJudgment = null;
+        [SerializeField] private GameObject desireGroup = null;
+        [SerializeField] private Text thoughtDesire = null;
+        [SerializeField] private Image facePicture = null;
         
         private RectTransform _transform;
         private Canvas _canvas;
@@ -15,11 +22,19 @@ namespace NPC {
         private void Awake() {
             _transform = GetComponent<RectTransform>();
             _canvas = GetComponent<Canvas>();
+            
+            tasteGroup.SetActive(true);
+            intentionGroup.SetActive(false);
+            judgmentGroup.SetActive(false);
+            desireGroup.SetActive(false);
         }
 
         public void ChangeContents(PasserbyDescription description) {
             fullName.text = $"{description.FirstName} {description.Name}";
-            thought.text = description.Thought;
+            thoughtTaste.text = description.Taste;
+            thoughtDesire.text = description.Desire;
+            thoughtIntention.text = description.Intention;
+            thoughtJudgment.text = description.Judgment;
             facePicture.sprite = description.FacePhoto;
         }
     }
