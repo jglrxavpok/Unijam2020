373da444658de103bf3d16d512be886bd0059992
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-11-15 12:04:46 +0100

    scene transition

--- Assets/Scripts/SceneControl/SceneControl.cs
+++ Assets/Scripts/SceneControl/SceneControl.cs
@@ -12,6 +12,12 @@ public class SceneControl : MonoBehaviour
     public float fadeTime;
 
     private float audioBoxIniSoundVolume;
+    private float audioBoxInMusicVolume;
+
+    private bool block = false;
+
+    private int fadeStep = -1;
+    private float time = 0;
 
     private void Awake () {
         if(Instance) {
@@ -23,7 +29,59 @@ public class SceneControl : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    private void Start () {
+        ChangeScene("JT");
+    }
+
     public void ChangeScene(string sceneName) {
+        if(block) return;
+
+        block = true;
+        StartCoroutine(FadeCoroutine(sceneName));
+    }
+
+    private IEnumerator FadeCoroutine (string sceneName) {
+        if(AudioBox.Instance){
+            audioBoxIniSoundVolume = AudioBox.Instance.SoundGlobalVolume;
+            audioBoxInMusicVolume = AudioBox.Instance.MusicGlobalVolume;
+        }
+
+        fadeStep = 0;
+        time = Time.time;
+        yield return new WaitForSeconds(fadeTime / 2);
+
+        fade.color = new Color(0, 0, 0, 1);
+        if(AudioBox.Instance){
+            AudioBox.Instance.StopAllSoundLoop();
+        }
         SceneManager.LoadScene(sceneName);
+        fadeStep = 1;
+        time = Time.time;
+        yield return new WaitForSeconds(fadeTime / 2);
+
+        fade.color = new Color(0, 0, 0, 0);
+        fadeStep = -1;
+    }
+
+    private void Update () {
+        float t = 0;
+        switch (fadeStep) {
+            case 0 :
+                t = (Time.time - time) / (fadeTime / 2);
+                fade.color = new Color(0, 0, 0, t);
+                if(AudioBox.Instance){
+                    AudioBox.Instance.SoundGlobalVolume = audioBoxIniSoundVolume * (1 - t);
+                    AudioBox.Instance.MusicGlobalVolume = audioBoxInMusicVolume * (1 - t);
+                }
+                break;
+            case 1 :
+                t = 1 - (Time.time - time) / (fadeTime / 2);
+                fade.color = new Color(0, 0, 0, t);
+                if(AudioBox.Instance){
+                    AudioBox.Instance.SoundGlobalVolume = audioBoxIniSoundVolume * (1 - t);
+                    AudioBox.Instance.MusicGlobalVolume = audioBoxInMusicVolume * (1 - t);
+                }
+                break;
+        }
     }
 }
