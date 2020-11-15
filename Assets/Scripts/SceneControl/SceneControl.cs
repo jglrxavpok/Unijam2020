a2179edc6866a73fd78336572bc34b550111cc93
Matteo BRANDI <matteo.brandi@ensiie.fr>
2020-11-15 11:25:43 +0100

    scene control in progress

---
+++ Assets/Scripts/SceneControl/SceneControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    static public SceneControl Instance {get; private set;}
+
+    [SerializeField]
+    private SpriteRenderer fade;
+    public float fadeTime;
+
+    private float audioBoxIniSoundVolume;
+
+    private void Awake () {
+        if(Instance) {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ChangeScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+}
